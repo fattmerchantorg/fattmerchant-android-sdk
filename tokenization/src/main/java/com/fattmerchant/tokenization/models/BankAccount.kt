adea787379a3d70ce02097994efca5aa386e93d1
Tulio Troncoso <tuliot@live.com>
2019-08-26 17:29:42 -0400

    Lint BankAccount and CreditCard files

--- tokenization/src/main/java/com/fattmerchant/tokenization/models/BankAccount.kt
+++ tokenization/src/main/java/com/fattmerchant/tokenization/models/BankAccount.kt
@@ -3,47 +3,47 @@ package com.fattmerchant.tokenization.models
 import com.squareup.moshi.Json
 
 /** A bank account */
-class BankAccount (
+class BankAccount(
 
-    @Json(name = "person_name")
-    var personName: String,
+        @Json(name = "person_name")
+        var personName: String,
 
-    @Json(name = "bank_type")
-    var bankType: String = "checkings",
+        @Json(name = "bank_type")
+        var bankType: String = "checkings",
 
-    @Json(name = "bank_holder_type")
-    var bankHolderType: String = "business",
+        @Json(name = "bank_holder_type")
+        var bankHolderType: String = "business",
 
-    @Json(name = "bank_account")
-    var bankAccount: String,
+        @Json(name = "bank_account")
+        var bankAccount: String,
 
-    @Json(name = "bank_routing")
-    var bankRouting: String,
+        @Json(name = "bank_routing")
+        var bankRouting: String,
 
-    @Json(name = "address_zip")
-    var addressZip: String,
+        @Json(name = "address_zip")
+        var addressZip: String,
 
-    @Json(name = "bank_name")
-    var bankName: String? = null,
+        @Json(name = "bank_name")
+        var bankName: String? = null,
 
-    @Json(name = "address_1")
-    var address1: String? = null,
+        @Json(name = "address_1")
+        var address1: String? = null,
 
-    @Json(name = "address_2")
-    var address2: String? = null,
+        @Json(name = "address_2")
+        var address2: String? = null,
 
-    @Json(name = "address_city")
-    var addressCity: String? = null,
+        @Json(name = "address_city")
+        var addressCity: String? = null,
 
-    @Json(name = "address_state")
-    var addressState: String? = null,
+        @Json(name = "address_state")
+        var addressState: String? = null,
 
-    @Json(name = "customer_id")
-    var customerId: String? = null,
+        @Json(name = "customer_id")
+        var customerId: String? = null,
 
-    var note: String? = null,
-    var phone: String? = null,
-    var email: String? = null
+        var note: String? = null,
+        var phone: String? = null,
+        var email: String? = null
 ) {
 
     private var method: String = "bank"
@@ -66,26 +66,22 @@ class BankAccount (
                 phone: String?,
                 email: String?,
                 method: String?
-    ): this(personName, bankType, bankHolderType, bankAccount, bankRouting, addressZip, bankName, address1, address2, addressCity, addressState, customerId, note, phone, email)
+    ) : this(personName, bankType, bankHolderType, bankAccount, bankRouting, addressZip, bankName, address1, address2, addressCity, addressState, customerId, note, phone, email)
 
     companion object {
-
         /**
          * Creates a test bank account
          *
          * @return a test bank account
          */
-        fun testBankAccount(): BankAccount {
-            val bankAccount = BankAccount(personName = "Jim Parsnip", bankType = "savings", bankAccount = "9876543210", bankRouting = "021000021", addressZip = "32822")
-            bankAccount.address1 = "123 Orange Ave"
-            bankAccount.address2 = "Unit 309"
-            bankAccount.addressCity = "Orlando"
-            bankAccount.addressState = "FL"
-            bankAccount.phone = "3210000000"
-            bankAccount.email = "fatt@merchant.com"
-            bankAccount.note = "This is a test credit card"
-
-            return bankAccount
+        fun testBankAccount() = BankAccount(personName = "Jim Parsnip", bankType = "savings", bankAccount = "9876543210", bankRouting = "021000021", addressZip = "32822").apply {
+            address1 = "123 Orange Ave"
+            address2 = "Unit 309"
+            addressCity = "Orlando"
+            addressState = "FL"
+            phone = "3210000000"
+            email = "fatt@merchant.com"
+            note = "This is a test credit card"
         }
 
         /**
@@ -93,19 +89,14 @@ class BankAccount (
          *
          * @return a test bank account
          */
-        fun failingTestBankAccount(): BankAccount {
-            val bankAccount = BankAccount(personName = "Jim Parsnip", bankAccount = "9876543210", bankRouting = "021000021", addressZip = "32822")
-
-            bankAccount.address1 = "123 Orange Ave"
-            bankAccount.address2 = "Unit 309"
-            bankAccount.addressCity = "Orlando"
-            bankAccount.addressState = "FL"
-            bankAccount.phone = "3210000000"
-            bankAccount.email = "fatt@merchant.com"
-            bankAccount.note = "This is a test credit card"
-
-            return bankAccount
+        fun failingTestBankAccount() = BankAccount(personName = "Jim Parsnip", bankAccount = "9876543210", bankRouting = "021000021", addressZip = "32822").apply {
+            address1 = "123 Orange Ave"
+            address2 = "Unit 309"
+            addressCity = "Orlando"
+            addressState = "FL"
+            phone = "3210000000"
+            email = "fatt@merchant.com"
+            note = "This is a test credit card"
         }
     }
-
 }
--- tokenization/src/main/java/com/fattmerchant/tokenization/models/CreditCard.kt
+++ tokenization/src/main/java/com/fattmerchant/tokenization/models/CreditCard.kt
@@ -3,38 +3,38 @@ package com.fattmerchant.tokenization.models
 import com.squareup.moshi.Json
 
 /** A credit card */
-class CreditCard (
+class CreditCard(
 
-    @Json(name = "person_name")
-    var personName: String,
+        @Json(name = "person_name")
+        var personName: String,
 
-    @Json(name = "card_number")
-    var cardNumber: String,
+        @Json(name = "card_number")
+        var cardNumber: String,
 
-    @Json(name = "card_exp")
-    var cardExp: String,
+        @Json(name = "card_exp")
+        var cardExp: String,
 
-    @Json(name = "address_zip")
-    var addressZip: String,
+        @Json(name = "address_zip")
+        var addressZip: String,
 
-    @Json(name = "address_1")
-    var address1: String? = null,
+        @Json(name = "address_1")
+        var address1: String? = null,
 
-    @Json(name = "address_2")
-    var address2: String? = null,
+        @Json(name = "address_2")
+        var address2: String? = null,
 
-    @Json(name = "address_city")
-    var addressCity: String? = null,
+        @Json(name = "address_city")
+        var addressCity: String? = null,
 
-    @Json(name = "address_state")
-    var addressState: String? = null,
+        @Json(name = "address_state")
+        var addressState: String? = null,
 
-    @Json(name = "customer_id")
-    var customerId: String? = null,
+        @Json(name = "customer_id")
+        var customerId: String? = null,
 
-    var note: String? = null,
-    var phone: String? = null,
-    var email: String? = null
+        var note: String? = null,
+        var phone: String? = null,
+        var email: String? = null
 ) {
 
     private var method: String = "card"
@@ -64,17 +64,14 @@ class CreditCard (
          *
          * @return a test credit card
          */
-        fun testCreditCard(): CreditCard {
-            val creditCard = CreditCard(personName = "Joan Parsnip", cardNumber = "4111111111111111", cardExp = "1230", addressZip = "32822")
-            creditCard.address1 = "123 Orange Ave"
-            creditCard.address2 = "Unit 309"
-            creditCard.addressCity = "Orlando"
-            creditCard.addressState = "FL"
-            creditCard.phone = "3210000000"
-            creditCard.email = "fatt@merchant.com"
-            creditCard.note = "This is a test credit card"
-
-            return creditCard
+        fun testCreditCard() = CreditCard(personName = "Joan Parsnip", cardNumber = "4111111111111111", cardExp = "1230", addressZip = "32822").apply {
+            address1 = "123 Orange Ave"
+            address2 = "Unit 309"
+            addressCity = "Orlando"
+            addressState = "FL"
+            phone = "3210000000"
+            email = "fatt@merchant.com"
+            note = "This is a test credit card"
         }
 
         /**
@@ -82,18 +79,14 @@ class CreditCard (
          *
          * @return a test credit card
          */
-        fun failingTestCreditCard(): CreditCard {
-            val creditCard = CreditCard(personName = "Joan Parsnip", cardNumber = "4111111111111111", cardExp = "", addressZip = "32822")
-            creditCard.address1 = "123 Orange Ave"
-            creditCard.address2 = "Unit 309"
-            creditCard.addressCity = "Orlando"
-            creditCard.addressState = "FL"
-            creditCard.phone = "3210000000"
-            creditCard.email = "fatt@merchant.com"
-            creditCard.note = "This is a test credit card"
-
-            return creditCard
+        fun failingTestCreditCard() = CreditCard(personName = "Joan Parsnip", cardNumber = "4111111111111111", cardExp = "", addressZip = "32822").apply {
+            address1 = "123 Orange Ave"
+            address2 = "Unit 309"
+            addressCity = "Orlando"
+            addressState = "FL"
+            phone = "3210000000"
+            email = "fatt@merchant.com"
+            note = "This is a test credit card"
         }
     }
-
 }
