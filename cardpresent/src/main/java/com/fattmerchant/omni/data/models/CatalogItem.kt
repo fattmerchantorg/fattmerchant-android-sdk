8abeceeeaf0a58391c20b5c43c01862527b95c37
Tulio Troncoso <tuliot@live.com>
2021-03-17 15:36:24 -0400

    [MOB-659] Add missing properties to CatalogItem

--- cardpresent/src/main/java/com/fattmerchant/omni/data/models/CatalogItem.kt
+++ cardpresent/src/main/java/com/fattmerchant/omni/data/models/CatalogItem.kt
@@ -7,4 +7,23 @@ open class CatalogItem: Model {
     open var details: String? = null
     open var quantity: Int = 1
     open var price: Double = 0.0
+
+    /** Is the item active */
+    open var isActive: Boolean? = null
+
+    /** Is the item taxable */
+    open var isTaxable: Boolean? = null
+
+    /** Is the item a service */
+    open var isService: Boolean? = null
+
+    /** Is the item a discount */
+    open var isDiscount: Boolean? = null
+
+    /** True only when this CatalogItem represents a percentage discount.
+     * For example, a 30% off discount */
+    open var isPercentage: Boolean? = null
+
+    /** Is the item discountable */
+    open var isDiscountable: Boolean? = null
 }
