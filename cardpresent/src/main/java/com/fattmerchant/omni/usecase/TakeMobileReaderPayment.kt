9827438b9a2a2aa30ab2049cc35f028d4cced0a3
Tulio Troncoso <tuliot@live.com>
2021-12-03 12:18:08 -0500

    [MOB-1087] Support customer_id in TakeMobileReaderPayment
    Also a mild refactor of that file to use the new object creation methods in TransactionResult

--- cardpresent/src/main/java/com/fattmerchant/omni/usecase/TakeMobileReaderPayment.kt
+++ cardpresent/src/main/java/com/fattmerchant/omni/usecase/TakeMobileReaderPayment.kt
@@ -1,7 +1,5 @@
 package com.fattmerchant.omni.usecase
 
-import com.fattmerchant.android.anywherecommerce.AWCDriver
-import com.fattmerchant.android.chipdna.ChipDnaDriver
 import com.fattmerchant.omni.SignatureProviding
 import com.fattmerchant.omni.TransactionUpdateListener
 import com.fattmerchant.omni.UserNotificationListener
@@ -28,43 +26,6 @@ internal class TakeMobileReaderPayment(
     class TakeMobileReaderPaymentException(message: String? = null) :
         OmniException("Could not take mobile reader payment", message)
 
-    companion object {
-        internal fun transactionMetaFrom(result: TransactionResult): Map<String, Any> {
-            val transactionMeta = mutableMapOf<String, Any>()
-
-            when {
-                result.source.contains(ChipDnaDriver().source) -> {
-                    result.userReference?.let {
-                        transactionMeta["nmiUserRef"] = it
-                    }
-
-                    result.localId?.let {
-                        transactionMeta["cardEaseReference"] = it
-                    }
-
-                    result.externalId?.let {
-                        transactionMeta["nmiTransactionId"] = it
-                    }
-                }
-
-                result.source.contains(AWCDriver().source) -> {
-                    result.externalId?.let {
-                        transactionMeta["awcTransactionId"] = it
-                    }
-                }
-            }
-
-            result.request?.lineItems?.let { transactionMeta["lineItems"] = it }
-            result.request?.subtotal?.let { transactionMeta["subtotal"] = it }
-            result.request?.tax?.let { transactionMeta["tax"] = it }
-            result.request?.tip?.let { transactionMeta["tip"] = it }
-            result.request?.memo?.let { transactionMeta["memo"] = it }
-            result.request?.reference?.let { transactionMeta["reference"] = it }
-
-            return transactionMeta
-        }
-    }
-
     suspend fun start(onError: (OmniException) -> Unit): Transaction? = coroutineScope {
 
         // Get the reader responsible for taking the payment
@@ -75,6 +36,19 @@ internal class TakeMobileReaderPayment(
         }
 
         var invoice = Invoice()
+        var customer: Customer? = null
+
+        // Try to get the customer by id. If we couldn't get it, throw an error
+        request.customerId?.let { customerId ->
+            val retrievedCustomer = customerRepository.getById(customerId) { }
+
+            if (retrievedCustomer != null) {
+                customer = retrievedCustomer
+            } else {
+                onError(TakeMobileReaderPaymentException("Could not retrieve customer with id $customerId"))
+                return@coroutineScope null
+            }
+        }
 
         // Check if we are passing an invoice id
         request.invoiceId?.let {
@@ -113,34 +87,20 @@ internal class TakeMobileReaderPayment(
             return@coroutineScope null
         }
 
-        val cardLastFour = try {
-            result.maskedPan?.substring(result.maskedPan!!.lastIndex - 3) ?: "****"
-        } catch (e: Error) {
-            "****"
-        }
-
         // Create customer
-        val customer = customerRepository.create(
-            Customer().apply {
-                firstname = if(result.transactionSource.equals("contactless", true)) "Mobile Device" else result.cardHolderFirstName ?: "SWIPE"
-                lastname = if(result.transactionSource.equals("contactless", true)) "Customer" else result.cardHolderLastName ?: "CUSTOMER"
-            }
-        ) {
-            onError(it)
-        } ?: return@coroutineScope null
+        if (customer == null) {
+            customer = customerRepository.create(
+                result.generateCustomer()
+            ) {
+                onError(it)
+            } ?: return@coroutineScope null
+        }
 
         // Create a PaymentMethod
         val paymentMethod = paymentMethodRepository.create(
-            PaymentMethod().apply {
-                merchantId = customer.merchantId
-                customerId = customer.id
-                method = "card"
-                cardType = result.cardType
-                cardExp = result.cardExpiration
-                this.cardLastFour = cardLastFour
-                personName = (customer.firstname ?: "") + " " + (customer.lastname ?: "")
-                tokenize = false
-                paymentToken = result.paymentToken
+            result.generatePaymentMethod().apply {
+                merchantId = customer?.merchantId
+                customerId = customer?.id
             }
         ) {
             onError(it)
@@ -148,7 +108,7 @@ internal class TakeMobileReaderPayment(
 
         // Associate payment method and invoice with customer
         invoice.paymentMethodId = paymentMethod.id
-        invoice.customerId = customer.id
+        invoice.customerId = customer?.id
 
         // Update invoice
         invoiceRepository.update(invoice) {
@@ -156,47 +116,11 @@ internal class TakeMobileReaderPayment(
         } ?: return@coroutineScope null
 
         // Create transaction
-        val transactionMeta = transactionMetaFrom(result)
-
-        var gatewayResponse: Map<String, Any>? = null
-
-        result.authCode?.let {
-            val responseMap = mapOf(
-                "gateway_specific_response_fields" to mapOf(
-                    "nmi" to mapOf(
-                        "authcode" to it
-                    )
-                )
-            )
-
-            gatewayResponse = responseMap
-        }
-
         transactionRepository.create(
-            Transaction().apply {
+            result.generateTransaction().apply {
                 paymentMethodId = paymentMethod.id
-                total = request.amount.dollarsString()
-                success = result.success
-                lastFour = cardLastFour
-                meta = transactionMeta
-                type = "charge"
-                method = "card"
-                source = "Android|CPSDK|${result.source}"
-                if(result.source == "AWC") {
-                    isRefundable = false
-                    isVoidable = false
-                }
-                customerId = customer.id
+                customerId = customer?.id
                 invoiceId = invoice.id
-                response = gatewayResponse
-                token = result.externalId
-
-                if (request.preauth) {
-                    preAuth = true
-                    isCaptured = 0
-                    isVoidable = true
-                    type = "pre_auth"
-                }
             }
         ) {
             onError(it)
