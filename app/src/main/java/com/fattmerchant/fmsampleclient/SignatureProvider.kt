05667e791eefb969460dbc0e8c98e46be8a4850d
Muraino Yakubu <muraino.yakubu@mavenmachines.com>
2022-08-11 10:30:48 +0100

    reformatting fattmerchant sdk and linting the source

--- app/src/main/java/com/fattmerchant/fmsampleclient/SignatureProvider.kt
+++ app/src/main/java/com/fattmerchant/fmsampleclient/SignatureProvider.kt
@@ -2,10 +2,9 @@ package com.fattmerchant.fmsampleclient
 
 import com.fattmerchant.omni.SignatureProviding
 
-class SignatureProvider: SignatureProviding {
+class SignatureProvider : SignatureProviding {
 
     override fun signatureRequired(completion: (String) -> Unit) {
         completion("signature")
     }
-
 }
