a1088bfae5ca1196f344f37c292dc1a7cfdbb225
Thomas Vidas <hello@thomasvidas.com>
2023-01-17 13:52:02 -0500

    feat: Add api gradle module

---
+++ api/src/test/java/com/staxpayments/api/CustomerUnitTest.kt
@@ -0,0 +1,16 @@
+package com.staxpayments.api
+
+import org.junit.Test
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CustomerUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
