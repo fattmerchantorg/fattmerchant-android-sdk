77c99850473854201a07e5874681ecd917642697
Muraino Yakubu <muraino.yakubu@mavenmachines.com>
2023-02-01 17:00:59 +0000

    LINTED THE FILE

--- api/src/main/java/com/staxpayments/api/datasource/CustomerLiveRepository.kt
+++ api/src/main/java/com/staxpayments/api/datasource/CustomerLiveRepository.kt
@@ -4,7 +4,7 @@ import com.staxpayments.api.models.Customer
 import com.staxpayments.api.network.NetworkClient
 import com.staxpayments.api.repository.CustomerRepository
 
-class CustomerLiveRepository(private val networkClients: NetworkClient): CustomerRepository {
+class CustomerLiveRepository(private val networkClients: NetworkClient) : CustomerRepository {
     override suspend fun customer(): Customer =
-        networkClients.get("https://api.",responseType = Customer::class.java)
+        networkClients.get("https://api.", responseType = Customer::class.java)
 }
--- api/src/main/java/com/staxpayments/api/repository/CustomerRepository.kt
+++ api/src/main/java/com/staxpayments/api/repository/CustomerRepository.kt
@@ -3,5 +3,5 @@ package com.staxpayments.api.repository
 import com.staxpayments.api.models.Customer
 
 interface CustomerRepository {
-   suspend fun customer(): Customer
+    suspend fun customer(): Customer
 }
