9c44ecca29e179df5b6eb74736dfa7357a17f66a
cjae <cjaeomon@gmail.com>
2023-02-06 14:09:20 +0100

    test refactor

--- api/src/test/java/com/staxpayments/api/repository/InvoiceRepositoryTest.kt
+++ api/src/test/java/com/staxpayments/api/repository/InvoiceRepositoryTest.kt
@@ -16,12 +16,27 @@ import org.junit.Test
  */
 class InvoiceRepositoryTest {
 
-    private var invoiceId: String? = null
+    private lateinit var invoiceId: String
     private lateinit var fakeRepository: InvoiceFakeRepository
 
+    private val fakeBody = CreateInvoiceBody(
+        total = 0.09,
+        url = "https://app.staxpayments.com/#/bill/",
+        meta = null,
+        files = null,
+        customerId = "ac097e4f-2a99-4689-8f90-29ecc2ff8455",
+        sendNow = true,
+        isPartialPaymentEnabled = true
+    )
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setUp() {
         fakeRepository = InvoiceFakeRepository()
+
+        runTest(UnconfinedTestDispatcher()) {
+            invoiceId = fakeRepository.createInvoice(fakeBody).id
+        }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -47,31 +62,27 @@ class InvoiceRepositoryTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun test_getInvoice() = runTest(UnconfinedTestDispatcher()) {
-        invoiceId?.let {
-            val response = fakeRepository.getInvoice(it)
+        val response = fakeRepository.getInvoice(invoiceId)
 
-            Assert.assertNotNull(response)
-            Assert.assertEquals(0.09, response?.total)
-        }
+        Assert.assertNotNull(response)
+        Assert.assertEquals(0.09, response?.total)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun test_updateInvoice() = runTest(UnconfinedTestDispatcher()) {
-        invoiceId?.let {
-            val body = CreateInvoiceBody(
-                total = 1.00,
-                url = "https://app.staxpayments.com/#/bill/",
-                meta = null,
-                files = null,
-                customerId = "ac097e4f-2a99-4689-8f90-29ecc2ff8455",
-                sendNow = true,
-                isPartialPaymentEnabled = false
-            )
+        val body = CreateInvoiceBody(
+            total = 1.00,
+            url = "https://app.staxpayments.com/#/bill/",
+            meta = null,
+            files = null,
+            customerId = "ac097e4f-2a99-4689-8f90-29ecc2ff8455",
+            sendNow = true,
+            isPartialPaymentEnabled = false
+        )
 
-            val response = fakeRepository.updateInvoice(it, body)
-            Assert.assertNotNull(response)
-            Assert.assertEquals(1.00, response?.total)
-        }
+        val response = fakeRepository.updateInvoice(invoiceId, body)
+        Assert.assertNotNull(response)
+        Assert.assertEquals(1.00, response?.total)
     }
 }
