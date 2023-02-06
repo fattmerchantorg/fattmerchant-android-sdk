2f24562c651e029e6400a1c9cf751e580b278b56
cjae <cjaeomon@gmail.com>
2023-02-06 15:13:15 +0100

    Code cleanup

--- api/src/main/java/com/staxpayments/api/repository/InvoiceRepository.kt
+++ api/src/main/java/com/staxpayments/api/repository/InvoiceRepository.kt
@@ -5,12 +5,9 @@ import com.staxpayments.api.requests.CreateInvoiceBody
 
 interface InvoiceRepository {
 
-    // READ
     suspend fun getInvoice(invoiceId: String): Invoice?
 
-    // WRITE
     suspend fun createInvoice(body: CreateInvoiceBody): Invoice
 
-    // UPDATE
     suspend fun updateInvoice(invoiceId: String, body: CreateInvoiceBody): Invoice?
 }
