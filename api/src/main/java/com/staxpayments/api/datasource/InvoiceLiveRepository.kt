af6d0d046acc87cb21d26504152d76e5a59c5dbd
cjae <cjaeomon@gmail.com>
2023-02-09 13:28:29 +0100

    fixes

--- api/src/main/java/com/staxpayments/api/datasource/InvoiceLiveRepository.kt
+++ api/src/main/java/com/staxpayments/api/datasource/InvoiceLiveRepository.kt
@@ -3,7 +3,7 @@ package com.staxpayments.api.datasource
 import com.staxpayments.api.models.Invoice
 import com.staxpayments.api.network.NetworkClient
 import com.staxpayments.api.repository.InvoiceRepository
-import com.staxpayments.api.requests.InvoicePostRequest
+import com.staxpayments.api.requests.InvoiceRequest
 
 class InvoiceLiveRepository(
     private val networkClients: NetworkClient
@@ -13,11 +13,11 @@ class InvoiceLiveRepository(
         return networkClients.get("invoice/$invoiceId", responseType = Invoice.serializer())
     }
 
-    override suspend fun createInvoice(body: InvoicePostRequest): Invoice {
+    override suspend fun createInvoice(body: InvoiceRequest): Invoice {
         return networkClients.post("invoice", request = body, responseType = Invoice.serializer())
     }
 
-    override suspend fun updateInvoice(invoiceId: String, body: InvoicePostRequest): Invoice {
+    override suspend fun updateInvoice(invoiceId: String, body: InvoiceRequest): Invoice {
         return networkClients.put("invoice/$invoiceId", request = body, responseType = Invoice.serializer())
     }
 }
--- api/src/main/java/com/staxpayments/api/repository/InvoiceRepository.kt
+++ api/src/main/java/com/staxpayments/api/repository/InvoiceRepository.kt
@@ -1,13 +1,13 @@
 package com.staxpayments.api.repository
 
 import com.staxpayments.api.models.Invoice
-import com.staxpayments.api.requests.InvoicePostRequest
+import com.staxpayments.api.requests.InvoiceRequest
 
 interface InvoiceRepository {
 
     suspend fun getInvoice(invoiceId: String): Invoice
 
-    suspend fun createInvoice(body: InvoicePostRequest): Invoice
+    suspend fun createInvoice(body: InvoiceRequest): Invoice
 
-    suspend fun updateInvoice(invoiceId: String, body: InvoicePostRequest): Invoice
+    suspend fun updateInvoice(invoiceId: String, body: InvoiceRequest): Invoice
 }
