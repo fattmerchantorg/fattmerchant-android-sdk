ac26157d8b94dc0f094c45bb69644bd270a3e63e
Thomas Vidas <hello@thomasvidas.com>
2023-03-08 11:48:26 -0500

    refactor: convert .gradle to .gradle.kts for tokenization module

---
+++ tokenization/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+group = "com.github.fattmerchantorg"
+
+android {
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 23
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.0.0")
+}
