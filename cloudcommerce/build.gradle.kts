0918939c98ca8c1e8f92dd118e125acff740164b
Hassan Nazari <hassan.nazari@staxpayments.com>
2026-02-04 19:38:29 -0500

    Add Maven publishing configuration to cloudcommerce module
    - Added maven-publish plugin to cloudcommerce module
    - Configured proper group/version from gradle.properties
    - Added POM metadata for JitPack distribution
    - Cloudcommerce module will now be published as a separate artifact
    - Fixes issue where Cloud Commerce SDK AAR was not included in JitPack build

--- cloudcommerce/build.gradle.kts
+++ cloudcommerce/build.gradle.kts
@@ -4,19 +4,47 @@
  * This module wraps the NMI Cloud Commerce SDK AAR file as required by
  * the official NMI documentation for Tap to Pay on Android.
  * 
- * This is a simple wrapper module that exposes the AAR as an artifact.
- * It avoids the Gradle limitation of "Direct local .aar file dependencies 
- * are not supported when building an AAR."
+ * This is a Maven publishing wrapper that exposes the AAR as an artifact
+ * for JitPack distribution. It avoids the Gradle limitation of "Direct
+ * local .aar file dependencies are not supported when building an AAR."
  * 
  * Reference: https://docs.nmi.com/docs/preparing-for-development-android
  */
+
+plugins {
+    `maven-publish`
+}
+
+group = findProperty("PUBLISH_GROUP_ID")?.toString() ?: "com.fattmerchant"
+version = findProperty("PUBLISH_VERSION")?.toString() ?: "1.0.0"
 
 // Create a default configuration that will be consumed by cardpresent module
 configurations.maybeCreate("default")
 
 // Add the AAR artifact to the default configuration
 // Use MTF (test) AAR by default for development
-artifacts.add("default", file("../cardpresent/libs/cloud-commerce-sdk-mtf-5.3.0.aar"))
+val aarFile = file("../cardpresent/libs/cloud-commerce-sdk-mtf-5.3.0.aar")
+artifacts.add("default", aarFile)
 
 // For production builds, uncomment this line and comment the MTF line above:
-// artifacts.add("default", file("../cardpresent/libs/cloud-commerce-sdk-5.3.0.aar"))
+// val aarFile = file("../cardpresent/libs/cloud-commerce-sdk-5.3.0.aar")
+
+// Maven publishing configuration for JitPack
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = findProperty("PUBLISH_GROUP_ID")?.toString() ?: "com.fattmerchant"
+            artifactId = "cloudcommerce"
+            version = findProperty("PUBLISH_VERSION")?.toString() ?: "1.0.0"
+            
+            // Publish the AAR file as the main artifact
+            artifact(aarFile)
+            
+            pom {
+                name.set("Cloud Commerce SDK")
+                description.set("NMI Cloud Commerce SDK AAR wrapper for Tap to Pay on Android")
+                url.set("https://github.com/fattmerchantorg/fattmerchant-android-sdk")
+            }
+        }
+    }
+}
