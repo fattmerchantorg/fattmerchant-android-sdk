1891bc651f2fd478a98bc918ea21290135f3efce
Alyssa De Metro <alyssa.demetro@fattmerchant.com>
2026-02-24 12:29:22 -0500

    feat(PHO-4692): Upgrade sqlcipher for 16kb page size compliance (#115)
    Co-authored-by: Hassan Nazari <hassan.nazari@staxpayments.com>

--- cardpresent/build.gradle.kts
+++ cardpresent/build.gradle.kts
@@ -55,16 +55,41 @@ android {
 
 dependencies {
     // NMI Dependencies
-    api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("net.zetetic:android-database-sqlcipher:4.5.0@aar")
-    implementation("androidx.sqlite:sqlite:2.2.0")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("net.zetetic:sqlcipher-android:4.9.0@aar")
+    implementation("androidx.sqlite:sqlite:2.1.0")
     implementation("com.jakewharton.timber:timber:4.7.1")
 
     // JSON Parsing
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
+
+    // HTTP / OkHttp
+    implementation("com.squareup.okhttp3:logging-interceptor:4.4.0")
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.4.0")
+
+    // RxJava
+    implementation("io.reactivex.rxjava3:rxjava:3.0.0")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+
+    // Google Play Services
+    implementation("com.google.android.play:integrity:1.6.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.android.gms:play-services-safetynet:18.1.0")
+
+    // AndroidX / Kotlin
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.0")
+
+    // Misc
+    implementation("commons-codec:commons-codec:1.11")
+    implementation("org.slf4j:slf4j-api:1.7.30")
 
     // Ktor
     implementation("io.ktor:ktor-client-core:2.3.6")
